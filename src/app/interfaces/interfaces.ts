2d323a64fc1e942b1894679cb25bebe6d2105e94
victoriavermo <victoriavermo@gmail.com>
2021-05-04 10:26:54 +0200

    Creation of interfaces: Square, Coord. and Rover

---
+++ src/app/interfaces/interfaces.ts
@@ -0,0 +1,17 @@
+export interface Square{
+  width: number;
+  height: number;
+
+}
+
+export interface Coordinates{
+  xWidth: number;
+  yHeight: number;
+}
+
+export interface Rover{
+  direction: 'L' | 'R' | 'A'; //Direction
+  orientation: 'N' | 'E' | 'S' | 'W';
+  coordinates: Coordinates;
+  succesTrip: boolean;
+}
